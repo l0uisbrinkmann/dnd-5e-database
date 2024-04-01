dea21a46f791d2efdaf8baf976b45d7d779beadf
Louis Brinkmann <mail@louisbrinkmann.com>
2024-04-01 13:26:07 +0200

    Initialized project with Spring Boot Initializer

---
+++ src/main/java/com/louisbrinkmann/dnd5edatabase/Dnd5eDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.louisbrinkmann.dnd5edatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd5eDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd5eDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/louisbrinkmann/dnd5edatabase/Dnd5eDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louisbrinkmann.dnd5edatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd5eDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
